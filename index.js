e77c922a74257ebaaa395d9d6bcba00019ec0a95
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-05-15 13:30:38 -0700

    Update index.js
    added arrow rotation for activated states

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let questionOne = document.getElementById("Q1");
 let answerOne = document.querySelector(".A1");
+let arrowOne = document.querySelector(".arrow1");
 
 let questionTwo = document.getElementById("Q2");
 let answerTwo = document.querySelector(".A2");
+let arrowTwo = document.querySelector(".arrow2");
 
 let questionThree = document.getElementById("Q3");
 let answerThree = document.querySelector(".A3");
+let arrowThree = document.querySelector(".arrow3");
 
 let questionFour = document.getElementById("Q4");
 let answerFour = document.querySelector(".A4");
+let arrowFour = document.querySelector(".arrow4");
 
 //
 
@@ -17,8 +21,10 @@ questionOne.addEventListener("click", faqQOne);
 function faqQOne() {
   if (answerOne.style.display == "none") {
     answerOne.style.display = "block";
+    arrowOne.style.rotate = "180deg";
   } else {
     answerOne.style.display = "none";
+    arrowOne.style.rotate = "0deg";
   }
 }
 //
@@ -28,8 +34,10 @@ questionTwo.addEventListener("click", faqQTwo);
 function faqQTwo() {
   if (answerTwo.style.display == "none") {
     answerTwo.style.display = "block";
+    arrowTwo.style.rotate = "180deg";
   } else {
     answerTwo.style.display = "none";
+    arrowTwo.style.rotate = "0deg";
   }
 }
 
@@ -40,8 +48,10 @@ questionThree.addEventListener("click", faqQThree);
 function faqQThree() {
   if (answerThree.style.display == "none") {
     answerThree.style.display = "block";
+    arrowThree.style.rotate = "180deg";
   } else {
     answerThree.style.display = "none";
+    arrowThree.style.rotate = "0deg";
   }
 }
 
@@ -52,7 +62,9 @@ questionFour.addEventListener("click", faqQFour);
 function faqQFour() {
   if (answerFour.style.display == "none") {
     answerFour.style.display = "block";
+    arrowFour.style.rotate = "180deg";
   } else {
     answerFour.style.display = "none";
+    arrowFour.style.rotate = "0deg";
   }
 }
