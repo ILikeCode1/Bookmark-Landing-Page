472c5f424aa64115a1343ebaccb773abe8de893c
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-05-29 12:28:30 -0700

    Update index.js
    updated FAQ section

--- index.js
+++ index.js
@@ -40,75 +40,6 @@ function mediaQuery(e) {
   }
 }
 
-// FAQ section
-let questionOne = document.getElementById("Q1");
-let answerOne = document.querySelector(".A1");
-let arrowOne = document.querySelector(".arrow1");
-
-let questionTwo = document.getElementById("Q2");
-let answerTwo = document.querySelector(".A2");
-let arrowTwo = document.querySelector(".arrow2");
-
-let questionThree = document.getElementById("Q3");
-let answerThree = document.querySelector(".A3");
-let arrowThree = document.querySelector(".arrow3");
-
-let questionFour = document.getElementById("Q4");
-let answerFour = document.querySelector(".A4");
-let arrowFour = document.querySelector(".arrow4");
-
-questionOne.addEventListener("click", faqQOne);
-
-function faqQOne() {
-  if (answerOne.style.display == "none") {
-    answerOne.style.display = "block";
-    arrowOne.style.rotate = "180deg";
-  } else {
-    answerOne.style.display = "none";
-    arrowOne.style.rotate = "0deg";
-  }
-}
-
-//
-questionTwo.addEventListener("click", faqQTwo);
-
-function faqQTwo() {
-  if (answerTwo.style.display == "none") {
-    answerTwo.style.display = "block";
-    arrowTwo.style.rotate = "180deg";
-  } else {
-    answerTwo.style.display = "none";
-    arrowTwo.style.rotate = "0deg";
-  }
-}
-
-//
-questionThree.addEventListener("click", faqQThree);
-
-function faqQThree() {
-  if (answerThree.style.display == "none") {
-    answerThree.style.display = "block";
-    arrowThree.style.rotate = "180deg";
-  } else {
-    answerThree.style.display = "none";
-    arrowThree.style.rotate = "0deg";
-  }
-}
-
-//
-
-questionFour.addEventListener("click", faqQFour);
-
-function faqQFour() {
-  if (answerFour.style.display == "none") {
-    answerFour.style.display = "block";
-    arrowFour.style.rotate = "180deg";
-  } else {
-    answerFour.style.display = "none";
-    arrowFour.style.rotate = "0deg";
-  }
-}
-
 // FEATURES section
 let featuresSelector1 = document.getElementById("features-selector-1");
 let featuresTab1 = document.querySelector(".feature-tab-1");
@@ -194,5 +125,86 @@ function tabThree() {
     featuresTab1.style.display = "none";
     featuresSelector1.style.color = "hsl(229, 8%, 60%)";
     featuresSelector1.style.borderBottom = "0.1rem solid lightgrey";
+  }
+}
+
+// FAQ section
+let questionOne = document.getElementById("Q1");
+let answerOne = document.querySelector(".A1");
+let arrowOne = document.querySelector(".arrow1");
+
+let questionTwo = document.getElementById("Q2");
+let answerTwo = document.querySelector(".A2");
+let arrowTwo = document.querySelector(".arrow2");
+
+let questionThree = document.getElementById("Q3");
+let answerThree = document.querySelector(".A3");
+let arrowThree = document.querySelector(".arrow3");
+
+let questionFour = document.getElementById("Q4");
+let answerFour = document.querySelector(".A4");
+let arrowFour = document.querySelector(".arrow4");
+
+questionOne.addEventListener("click", faqQOne);
+
+function faqQOne() {
+  if (answerOne.style.display == "none") {
+    questionOne.style.borderBottom = "none";
+    answerOne.style.display = "block";
+    answerOne.style.borderBottom = "0.1rem solid lightgrey";
+    arrowOne.style.rotate = "180deg";
+  } else {
+    questionOne.style.borderBottom = "0.1rem solid lightgrey";
+    answerOne.style.display = "none";
+    arrowOne.style.rotate = "0deg";
+  }
+}
+
+//
+questionTwo.addEventListener("click", faqQTwo);
+
+function faqQTwo() {
+  if (answerTwo.style.display == "none") {
+    questionTwo.style.borderBottom = "none";
+    answerTwo.style.display = "block";
+    answerTwo.style.borderBottom = "0.1rem solid lightgrey";
+    arrowOne.style.rotate = "180deg";
+  } else {
+    questionTwo.style.borderBottom = "0.1rem solid lightgrey";
+    answerTwo.style.display = "none";
+    arrowTwo.style.rotate = "0deg";
+  }
+}
+
+//
+questionThree.addEventListener("click", faqQThree);
+
+function faqQThree() {
+  if (answerThree.style.display == "none") {
+    questionThree.style.borderBottom = "none";
+    answerThree.style.display = "block";
+    answerThree.style.borderBottom = "0.1rem solid lightgrey";
+    arrowThree.style.rotate = "180deg";
+  } else {
+    questionThree.style.borderBottom = "0.1rem solid lightgrey";
+    answerThree.style.display = "none";
+    arrowThree.style.rotate = "0deg";
+  }
+}
+
+//
+
+questionFour.addEventListener("click", faqQFour);
+
+function faqQFour() {
+  if (answerFour.style.display == "none") {
+    questionFour.style.borderBottom = "none";
+    answerFour.style.display = "block";
+    answerFour.style.borderBottom = "0.1rem solid lightgrey";
+    arrowFour.style.rotate = "180deg";
+  } else {
+    questionFour.style.borderBottom = "0.1rem solid lightgrey";
+    answerFour.style.display = "none";
+    arrowFour.style.rotate = "0deg";
   }
 }
